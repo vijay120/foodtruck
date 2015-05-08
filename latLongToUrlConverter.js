75411c733d8fbaa2cdb219c91771d09182e18e8c
vramakrishnan <vramakrishnan@box.com>
2015-05-08 00:35:33 -0700

    added minimal style to the app

--- latLongToUrlConverter.js
+++ latLongToUrlConverter.js
@@ -1,13 +1,15 @@
+var ADJUSTABLEZOOM = 0.5;
+
 module.exports = {
 	convertLatLongToAPIUrl: function(latLong) {
 		var latLongArray = latLong.split(','),
 			latitude = parseFloat(latLongArray[0]);
 	    	longitude = parseFloat(latLongArray[1]);
 
-	    var maxLat = latitude + 0.5,
-	    	minLat = latitude - 0.5,
-	    	maxLong = longitude + 0.5,
-	    	minLong = longitude - 0.5;
+	    var maxLat = latitude + ADJUSTABLEZOOM,
+	    	minLat = latitude - ADJUSTABLEZOOM,
+	    	maxLong = longitude + ADJUSTABLEZOOM,
+	    	minLong = longitude - ADJUSTABLEZOOM;
 
 	    var baseUrl = 'https://data.sfgov.org/resource/rqzj-sfat.json?$where=';
 	    baseUrl += 'latitude < ' + maxLat + ' AND '
